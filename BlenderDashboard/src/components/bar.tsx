ffc545bc56c1aa663fbbb5853b5f94da82f99271
TheDuit <dgls1122@gmail.com>
2024-05-26 03:52:56 -0300

    modificada chamada de api e renderização do componente para trazer dados da api python

--- BlenderDashboard/src/components/bar.tsx
+++ BlenderDashboard/src/components/bar.tsx
@@ -1,24 +1,38 @@
 import styled from "styled-components";
 
 const Barst = styled.div`
+    display: flex;
     height: 2rem;
     border-radius: .5rem;
     width: ${(props)=> props.scoreWidth /10 + 'px'};
-    background:rgba(123,188,255,1);
-    transition: ease-in-out 200ms;
+    background:#E37200;
     position: relative;
+    align-items: center;
+    justify-content: flex-start;
     
     &:hover{
-        padding-right: 1rem;
-        background:#0167ce;
+        background:#cd5200;
         transition: ease-in-out 200ms;
     }
+
+    & label{
+        position: absolute;
+        font-weight: bolder;
+        transition: ease-in-out 200ms;
+        color: #ffffff;
+    }
+
+    &:hover label{
+        transition: ease-in-out 200ms;
+        color: #fff;
+    }
+
 
 `;
 
 const Bar = ({score}:any)=>{
     return(
-        <Barst scoreWidth={score}></Barst>
+        <Barst scoreWidth={score}><label>{score} Points </label></Barst>
     );
 }
 
--- BlenderDashboard/src/components/dataLoader.tsx
+++ BlenderDashboard/src/components/dataLoader.tsx
@@ -1,50 +1,75 @@
-import blenderData from './blenderData';
-import styled from 'styled-components';
-import Bar from './bar.tsx'
+import styled from "styled-components";
+import { useEffect, useState } from "react";
+import Bar from "./bar";
 
 const TableSt = styled.table`
-    width:100dvw;
-    background-color: #001719;
+  width: -webkit-fill-available;
+  background-color: #001719;
+  margin: auto;
+  color: #fff;
+  font-family: sans-serif;
+  position: relative;
+  left:0;
 
-    & tr{
-        height: 2.5rem;
-        padding:1rem;
-    }
+  & tr {
+    height: 2.5rem;
+    padding: 1rem;
+    vertical-align: middle;
+  }
 
-    & td{
-        text-align: end;
-    }
+  & td {
+    width: fit-content;
+    text-align: center;
+  }
 
-    & label{
-        padding:1rem;
-        color:#fff;
-        font-family: sans-serif;
-    }
+  & label {
+    padding: 1rem;
+  }
 `;
 
-const BlenderDataLoader = ()=>{
-    return(
-      <TableSt>
-        <thead>
+const BlenderDataLoader = () => {
+  const [blenderData, setBlenderData] = useState(null);
+
+  useEffect(() => {
+    fetch('https://blenderopendatacaller.onrender.com/api/blenderData')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Falha na solicitação.');
+        }
+        return response.json();
+      })
+      .then(data => {
+        setBlenderData(data)
+      })
+      .catch(error => {
+        console.error('Erro:', error)
+      })
+  }, []);
+
+  return (
+    <div>
+    <TableSt>
+      <thead>
+        <tr>
+          {blenderData && blenderData.columns.map((column, index) => (
+            <td key={index}>{column.display_name}</td>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+      {blenderData && blenderData.rows.map((column, index) => (
+            index <=50 &&
             <tr>
-                <th>Name</th>
-                <th>Score</th>
+              <td>{column[0]}</td>
+              <td>{column[1]}</td>
+              <Bar score={column[2]}/>
+              <td>{column[3]}</td>
             </tr>
-                </thead>
-        {blenderData.map(item=>{
-            return(
-            <tr>
-                <td>
-                    <label>{item.name}</label>
-                </td>
-                <td>
-                    <Bar score={item.score} />
-                </td>
-            </tr>
-            )
-        })}
-      </TableSt>
-    )
-}
+          ))}
+      </tbody>
+    </TableSt>
+  </div>
+  );
+};
 
 export default BlenderDataLoader;
